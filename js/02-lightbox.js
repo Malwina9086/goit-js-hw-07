f96dd7294f01c829a736a3fcc23ff96d4929ed26
Malwina9086 <malwina.lukjanska1@gmail.com>
2023-08-27 11:36:37 +0100

    done all 2 tasks hw 07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listGallery = document.querySelector(".gallery");
+
+const galleryElements = [];
+galleryItems.forEach((item) => {
+  const element = `<li><a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></li>`;
+  galleryElements.push(element);
+});
+listGallery.innerHTML = galleryElements.join("");
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
